c3e2364d79a0fa034050fdea32de0061195df2f8
Eduardo Coimbra <eduardocoimbramag@gmail.com>
2025-09-04 02:15:00 -0300

    add-button-functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,11 @@ const pokemonId = document.querySelector("#poke-id")
 const pokemonImage = document.querySelector(".pokemon-image")
 const pokemonSearch = document.querySelector(".form")
 const pokemonInput = document.querySelector(".poke-input")
+const pokemonBtnPrev = document.querySelector("#button-prev")
+const pokemonBtnNext = document.querySelector("#button-next")
 
 async function fetchPokemon(pokemon) {
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`)
     const data = await APIResponse.json()
     return data
 }
@@ -16,13 +18,24 @@ async function renderPokemon(pokemon) {
     pokemonName.innerHTML = data.name
     pokemonId.innerHTML = data.id + "&nbsp;"
     pokemonImage.src = data.sprites.versions["generation-v"]["black-white"].animated.front_default
-
+    pokemonInput.value = ""
 }
 
 function submitForm(event) {
     event.preventDefault()
     renderPokemon(pokemonInput.value)
-    pokemonInput.value = ""
+}
+
+function prevClick() {
+    alert("Prev Clicked")
+}
+
+function nextClick() {
+    alert("Prev Clicked")
 }
 
 pokemonSearch.addEventListener("submit", submitForm)
+pokemonBtnPrev.addEventListener("click", prevClick)
+pokemonBtnNext.addEventListener("click", nextClick)
+
+renderPokemon("1")
